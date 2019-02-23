05eab382f595257c9ebd744263c8e9bd77de9505
Shreeji1507 <shreejibharatkumar.patel@dcmail.ca>
2019-02-23 14:25:20 -0500

    9 done

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,17 @@ router.get('/user/profile', function(req, res, next) {
   res.render('user/profile'); 
 });
 
+router.get('/user/signin', function(req, res, next) {
+  var messages = req.flash("error");
+  res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0}); 
+});
+
+router.post('/user/signup', passport.authenticate('local.signin', {
+  successRedirect: '/user/profile', 
+  failureRedirect: '/user/signin',
+  failureFlash: true
+}));
+
 module.exports = router;
 
 // this is a test
