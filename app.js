0c3073f26ecd236081f8669ec4321a962ef6305b
Shreeji1507 <shreejibharatkumar.patel@dcmail.ca>
2019-02-23 13:27:24 -0500

    7 done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 //var expressPug = require('express-handlebars');
 var mongoose = require("mongoose");
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping', {useNewUrlParser: true});
+require("./config/passport");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +27,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: "mysecret", resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
