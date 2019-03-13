ae472e46bdf57c7397577ed0492ed0c5a63a27ca
Shreeji1507 <shreejibharatkumar.patel@dcmail.ca>
2019-03-13 17:02:20 -0400

    hello

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,15 @@ router.get('/shopping-cart', function(req, res, next) {
   var cart = new Cart(req.session.cart);
   res.render("shop/shopping-cart", {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
+
+router.get('/checkout', function(req, res, next){
+  if (!req.session.cart){
+    return res.redirect("shop/shopping-cart");
+  }
+  var cart = new Cart(req.session.cart);
+  res.render("shop/checkout", {total: cart.totalPrice});
+
+});
 module.exports = router;
 
 // this is a test
