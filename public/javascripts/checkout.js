89786b7e88ba922e7fd946f592595c53a57c9572
Shreeji1507 <shreejibharatkumar.patel@dcmail.ca>
2019-03-14 23:51:34 -0400

    some stripe done

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -29,6 +29,31 @@ var card = elements.create('card', {
 // Add an instance of the card UI component into the `card-element` <div>
 card.mount('#card-element');
 
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the customer that there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
 function stripeTokenHandler(token) {
   // Insert the token ID into the form so it gets submitted to the server
   var form = document.getElementById('payment-form');
@@ -41,32 +66,3 @@ function stripeTokenHandler(token) {
   // Submit the form
   form.submit();
 }
-
-function createToken() {
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server
-      stripeTokenHandler(result.token);
-    }
-  });
-};
-
-// Create a token when the form is submitted.
-var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  createToken();
-});
-
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
